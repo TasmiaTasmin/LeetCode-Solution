fcac25e88812d6e8edf28995db8e1478d4047717
TasmiaTasmin <tasmintasmia676@gmail.com>
2026-01-02 11:10:54 +0600

    config

--- SortAnArrayOf0's1's2's/obj/Debug/net9.0/SortAnArrayOf0's1's2's.AssemblyInfo.cs
+++ SortAnArrayOf0's1's2's/obj/Debug/net9.0/SortAnArrayOf0's1's2's.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SortAnArrayOf0\'s1\'s2\'s")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bd31bb37c468f370de0544c039bcd5834667854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2f9873ac064be9e6208f62ccfeb2957d539db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SortAnArrayOf0\'s1\'s2\'s")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SortAnArrayOf0\'s1\'s2\'s")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TwoSum/obj/Debug/net7.0/TwoSum.AssemblyInfo.cs
+++ TwoSum/obj/Debug/net7.0/TwoSum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwoSum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bd31bb37c468f370de0544c039bcd5834667854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2f9873ac064be9e6208f62ccfeb2957d539db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwoSum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwoSum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
